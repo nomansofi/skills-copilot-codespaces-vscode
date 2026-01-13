96f45614e9b6b5b41684d9d5c4d138273c40e3b7
Noman Hassan Sofi <sofinoman6@gmail.com>
2026-01-13 05:50:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember()
+{
+    var skills = [
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "React",
+        "Node.js",
+        "Express",
+        "MongoDB",
+        "Git",
+        "RESTful APIs",
+        "TypeScript"
+    ];
+
+    return skills;
+}
