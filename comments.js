f5a518d82d6dd3397b2ccae147f1b7f99cfca915
Noman Hassan Sofi <sofinoman6@gmail.com>
2026-01-13 05:54:58 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+// create web server
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+    response.end('Hello, World!');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('Error occurred:', err);
+    }
+    console.log(`Server is listening on port ${port}`);
+}); 
+// To run the server, use the command: node <filename>.js
